c37370d5c3f68816fb7efa795cdac3aadb52cf02
Stelios Tsallas <109072027+st573york@users.noreply.github.com>
2022-07-14 16:13:08 +0300

    Update movie.js

--- src/js/movie.js
+++ src/js/movie.js
@@ -5,13 +5,34 @@ function voteMovie( action, movieid )
         url: "/ajax/process-movie.php",
         data: {
             'action': action,
-            'movieid': movieid
+            'movieid': movieid        
         },
         dataType: 'json',
         cache: false,
         success: function( data )
         {
-            window.location.reload();
+            if( data.resp )
+            {
+                // Update vote number
+                $( 'span#like_votes_' + movieid ).html( data.like_votes + ' likes' );
+                $( 'span#hate_votes_' + movieid ).html( data.hate_votes + ' hates' );
+
+                // Toggle vote link
+                if( action == 'like' ) 
+                {
+                    $( 'span#like_link_' + movieid ).html( 'Like' );
+                    $( 'span#hate_link_' + movieid ).html(
+                        "<a href='javascript:voteMovie( \"hate\", \"" + movieid + "\" )'>Hate</a>"
+                    );
+                }
+                else if( action == 'hate' )
+                {
+                    $( 'span#hate_link_' + movieid ).html( 'Hate' );
+                    $( 'span#like_link_' + movieid ).html(
+                        "<a href='javascript:voteMovie( \"like\", \"" + movieid + "\" )'>Like</a>"
+                    );
+                }
+            }
     	}             
     });
 }
@@ -23,11 +44,13 @@ function sortMovies( action, can_vote )
         url: "/ajax/process-movie.php",
         data: { 
             'action': action, 
-            'can_vote': can_vote },
+            'can_vote': can_vote 
+        },
         dataType: 'html',
         cache: false,
         success: function( data )
         {                 
+            // Update movie list
             $( '.movie_list' ).empty();
             $( '.movie_list' ).append( data );               
         }        
