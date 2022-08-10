6bedaf54e72cf6782c7ad467cd7bed2ad21f2db7
Stelios Tsallas <steliostsallas@gmail.com>
2022-08-10 18:50:38 +0300

    fixed revision number

--- src/conf/version.php
+++ src/conf/version.php
@@ -1,14 +1,20 @@
 <?php
 
+function getRevision()
+{
+    $status = @shell_exec( 'svnversion ' . dirname( __DIR__, 1 ) );
+    if( preg_match( '/(\d+)M/', $status, $matches ) ) {
+        return $matches[1];
+    }
+
+    return 0;
+}
+
 function getVersion()
 {   
     global $author, $product_name, $product_version;
-
-    $status = @shell_exec( 'svnversion ' . dirname( __DIR__, 1 ) );
-    preg_match( '/\d+/', $status, $match );
-    $revision = ( !empty( $match ) )? $match[0] : 0;
-
-    return "&copy; $author - $product_name v. {$product_version}.{$revision}";
+    
+    return "&copy; $author - $product_name v. {$product_version}." . getRevision();
 }
 
 ?>
